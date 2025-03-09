45bc519e3ddf55515208403a35e06afdc3ce4a36
Isaac Manríquez <isaacmb.tec@gmail.com>
2025-03-08 19:27:32 -0600

    agregamos variables de entorno

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
 import pg from 'pg';
+import {config} from 'dotenv';
+
+const dataBaseURI = process.env.DATABASE_URL || 'postgresql://user:password@localhost:5432/database';
 
 
 const { Pool } = pg;
-export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'api_ecommerce',
-    password: 'password',
-    port: 5432,
-});
+// export const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'api_ecommerce',
+//     password: 'password',
+//     port: 5432,
+// });
 
+export const pool = new Pool({
+    connectionString: dataBaseURI  
+})
